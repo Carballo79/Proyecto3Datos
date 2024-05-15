5d87eec923c3396500159ddf151adaa360ebd474
jadrian-cm <carballojose.im@gmail.com>
2024-05-14 23:36:19 -0600

    Primer commit

---
+++ Proyecto3/Proyecto3/VentanaPrincipal.cpp
@@ -0,0 +1,12 @@
+#include "VentanaPrincipal.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Proyecto3::VentanaPrincipal form;
+	Application::Run(% form);
+}
